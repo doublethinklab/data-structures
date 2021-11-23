27eaa9edc02cfa989fb6491190fc5d5f54905385
Tim Niven <tim.niven.public@gmail.com>
2021-11-23 20:30:51 +0800

    fix bug in setup

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import setuptools
 
 with open('version') as f:
     version = f.read().strip()
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
 
 
 setuptools.setup(
@@ -15,5 +13,4 @@ setuptools.setup(
     description='Explicitly defined data structures.',
     url=f'https://github.com/doublethinklab/data-structures.git#{version}',
     packages=setuptools.find_packages(),
-    python_requires='>=3.9.5',
-    install_requires=required)
+    python_requires='>=3.9.5')
